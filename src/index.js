402d7d8200a537b762f5eda15790e375113fd32e
Saul van der Walt <saulvdw@gmail.com>
2018-01-10 01:07:30 +0200

    smaller size + more idiomatic + StandardJS style
    ``each`` simplifies all the looping. ``isObj`` also just makes everything cleaner. Batcher is smaller with this most fanciful new runAsync function using async/await, lol. Yeah and some basic style changes and simplifications. Your style was so close to StandardJS anyways so I just auto formatted the lot to fit StandardJS; because eh, you know why not.
    The test you wrote (nice work btw) ran just fine.

--- src/index.js
+++ src/index.js
@@ -1,117 +1,93 @@
+const isObj = o => typeof o === 'object'
+
+const each = (obj, fun) => {
+  if ('forEach' in obj) obj.forEach(fun)
+  else {
+    const iterator = isObj(obj) ? Object.entries(obj) : obj
+    for (const [key, val] of iterator) fun(val, key, obj)
+  }
+  return obj
+}
+
 const computedStack = []
 const observersMap = new WeakMap()
 
-const computed = function(fun, { autoRun = true, callback = null } = {}) {
-    const proxy = new Proxy(fun, {
-        apply: function(target, thisArg, argsList) {
-            const performComputation = (fun = null) => {
-                computedStack.unshift(callback || proxy)
-                const result = fun ? fun() : target.apply(thisArg, argsList)
-                computedStack.shift()
-                return result
-            }
+const computed = (fun, {autoRun = true, callback = null} = {}) => {
+  const proxy = new Proxy(fun, {
+    apply (target, context, args) {
+      const performComputation = (fun = null) => {
+        computedStack.unshift(callback || proxy)
+        const result = fun ? fun() : target.apply(context, args)
+        computedStack.shift()
+        return result
+      }
 
-            argsList.push({
-                computeAsync: target => performComputation(target)
-            })
-
-            return performComputation()
-        }
-    })
-    if(autoRun) { proxy() }
-    return proxy
+      args.push({computeAsync: performComputation})
+      return performComputation()
+    }
+  })
+  if (autoRun) proxy()
+  return proxy
 }
 
-const dispose = _ => _.__disposed = true
+const dispose = o => { o.__disposed = true }
 
-const batcher = {
-    timeout: null,
-    queue: new Set(),
-    process: () => {
-        for(let task of batcher.queue)
-            task()
-        batcher.queue.clear()
-        batcher.timeout = null
+const runAsync = async f => (await new Promise(resolve => resolve(f)))()
+
+const batcher = {
+  queue: new Set(),
+  process () {
+    each(batcher.queue, task => task()).clear()
+  },
+  enqueue (task) {
+    runAsync(batcher.process)
+    batcher.queue.add(task)
+  }
+}
+
+const observe = (obj, options = {}) => {
+  const {props = null, ignore = null, batch = false, deep = false} = options
+  observersMap.set(obj, new Map())
+
+  deep && each(obj, (val, key) => {
+    if (isObj(val)) obj[key] = observe(val, options)
+  })
+
+  return new Proxy(obj, {
+    get (_, prop) {
+      if ((props && !props.includes(prop)) || (ignore && ignore.includes(prop))) {
+        return obj[prop]
+      }
+
+      const observerMap = observersMap.get(obj)
+      if (!observerMap.has(prop)) observerMap.set(prop, new Set())
+      if (computedStack.length) observerMap.get(prop).add(computedStack[0])
+
+      return obj[prop]
     },
-    enqueue: task => {
-        if(batcher.timeout === null)
-            batcher.timeout = setTimeout(batcher.process, 0)
-        batcher.queue.add(task)
-    }
-}
+    set (_, prop, val) {
+      if (obj[prop] === val) return true
 
-const observe = function(obj, options = {}) {
-    const { props = null, ignore = null, batch = false, deep = false } = options
-    observersMap.set(obj, new Map)
+      obj[prop] = deep && !(prop in obj) && isObj(val) ? observe(val, options) : val
 
-    if(deep) {
-        if(obj instanceof Array) {
-            for(const key in obj) {
-                if(typeof obj[key] === 'object')
-                    obj[key] = observe(obj[key], options)
-            }
-        } else {
-            for(let key in obj) {
-                if(!obj.hasOwnProperty(key))
-                    continue
-                const value = obj[key]
-                if(typeof value === 'object') {
-                    obj[key] = observe(value, options)
-                }
-            }
+      if ((props && !props.includes(prop)) || (ignore && ignore.includes(prop))) {
+        return true
+      }
+
+      const dependents = observersMap.get(obj).get(prop)
+      dependents && each(dependents, dependent => {
+        if (dependent.__disposed) dependents.delete(dependent)
+        else if (dependent !== computedStack[0]) {
+          batch ? batcher.enqueue(dependent) : dependent()
         }
+      })
+      return true
     }
-
-    return new Proxy(obj, {
-        get(_, prop) {
-            const observerMap = observersMap.get(obj)
-
-            if((props && !props.includes(prop)) || (ignore && ignore.includes(prop)))
-                return obj[prop]
-
-            if(!observerMap.has(prop)) {
-                observerMap.set(prop, new Set())
-            }
-
-            const set = observerMap.get(prop)
-            if(computedStack.length > 0)
-                set.add(computedStack[0])
-            observerMap.set(prop, set)
-
-            return obj[prop]
-        },
-        set(_, prop, value) {
-            const observerMap = observersMap.get(obj)
-
-            if(obj[prop] === value)
-                return true
-
-            if(deep && !(prop in obj) && typeof value === 'object')
-                obj[prop] = observe(value, options)
-            else
-                obj[prop] = value
-
-            if((props && !props.includes(prop)) || (ignore && ignore.includes(prop)))
-                return true
-
-            if(observerMap.has(prop)) {
-                const dependents = observerMap.get(prop)
-                for(let dependent of dependents) {
-                    if(dependent.__disposed) {
-                        dependents.delete(dependent)
-                    } else if(computedStack.length < 1 || dependent !== computedStack[0]) {
-                        if(batch) batcher.enqueue(dependent)
-                        else dependent()
-                    }
-                }
-            }
-            return true
-        }
-    })
+  })
 }
 
 export default {
-    observe,
-    computed,
-    dispose
+  observe,
+  computed,
+  dispose
 }
