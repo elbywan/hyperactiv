7d988f323d2b160f7eede91f50011e084ef69822
Julien Elbaz <elbywan@hotmail.com>
2018-02-04 10:23:24 +0100

    eslint improvements

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
-import uglify from "rollup-plugin-uglify"
-import { minify } from "uglify-es"
+import uglify from 'rollup-plugin-uglify'
+import { minify } from 'uglify-es'
 
 export default {
-    input: "./src/index.js",
+    input: './src/index.js',
     output: {
-        file: "dist/index.js",
-        format: "umd",
-        name: "hyperactiv"
+        file: 'dist/index.js',
+        format: 'umd',
+        name: 'hyperactiv'
     },
     plugins: [
         uglify({}, minify)
-    ],
+    ]
 }
