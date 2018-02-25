f71295f523fefb52a45b4f5216404341eb8b34c8
Jonathan Sterling Hollinger <src@jonholl.com>
2018-02-24 18:07:18 -0700

    Don't deep clone functions or values in write handler

--- src/index.js
+++ src/index.js
@@ -145,7 +145,7 @@ const getWriteContext = function(prop) {
 const writeHandler = function(target) {
     if(!target) throw new Error('writeHandler needs a proper target !')
     return function(props, value) {
-        value = JSON.parse(JSON.stringify(value))
+        value = isObj(value) ? JSON.parse(JSON.stringify(value)) : value
         for(let i = 0; i < props.length - 1; i++) {
             const prop = props[i], nextProp = props[i + 1]
             if(typeof target[prop] === 'undefined') target[prop] = getWriteContext(nextProp)
