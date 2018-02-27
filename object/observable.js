1d54023721a8b6d429b8e55ee205ff2ccd628b7e
Jonathan Sterling Hollinger <src@jonholl.com>
2018-02-26 18:38:19 -0700

    Move out of src

--- object/observable.js
+++ object/observable.js
@@ -1,6 +1,6 @@
 /* An extensible observed object */
 
-const { observe, computed, dispose } = require('../index')
+const { observe, computed, dispose } = require('./dist')
 module.exports = class Observable {
     constructor(data, options) {
         Object.defineProperty(this, '__computedProperties', { value: [ ], enumerable: false })
