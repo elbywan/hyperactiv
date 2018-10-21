d89f400374b968e0ddc8a549198b73f6bdfc76f9
elbywan <elbywan@hotmail.com>
2018-10-21 16:15:55 +0200

    improve react component

--- src/react/index.js
+++ src/react/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent, Fragment } from 'react'
+import { PureComponent } from 'react'
 import hyperactiv from 'hyperactiv'
 
 const { observe, computed, dispose } = hyperactiv
@@ -6,44 +6,39 @@ const { observe, computed, dispose } = hyperactiv
 export class Watch extends PureComponent {
     constructor(props) {
         super(props)
-        this.state = {}
+        this._callback = () => {
+            this.__mounted &&
+            this.forceUpdate.bind(this)()
+        }
+        this.state = {
+            render: this.computeRenderMethod(props.render)
+        }
     }
     componentWillUnmount() {
         this.__mounted = false
         if(this.state.render) {
             dispose(this.state.render)
+            dispose(this._callback)
         }
     }
     componentDidMount() {
         this.__mounted = true
-        this.refreshWatcherInState()
     }
-    componentDidUpdate(prevProps) {
-        if(prevProps.render !== this.props.render) {
-            dispose(prevProps.render)
-            this.refreshWatcherInState()
-        }
-    }
-    refreshWatcherInState() {
-        if(this.props.render) {
-            this.setState({
-                render: computed(this.props.render, {
-                    autoRun: false,
-                    callback: () => {
-                        this.__mounted &&
-                        this.forceUpdate.bind(this)()
-                    }
-                })
+    computeRenderMethod(newRender) {
+        if(this._currentRender !== newRender) {
+            if(!newRender)
+                return null
+            this._currentRender = computed(newRender, {
+                autoRun: false,
+                callback: this._callback
             })
         }
+        return this._currentRender
     }
     render() {
-        const { render } = this.state
-        return React.createElement(
-            Fragment,
-            [],
-            render && render()
-        )
+        const { render } = this.props
+        this.computeRenderMethod(render)
+        return this._currentRender && this._currentRender() || null
     }
 }
 
