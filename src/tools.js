538e22cce71ef561ca83d7ab0292e6e6764aabbc
elbywan <elbywan@hotmail.com>
2019-05-25 11:30:23 +0200

    code splitting

--- src/tools.js
+++ src/tools.js
@@ -1,7 +1,15 @@
-export default {
+const tools = {
     isObj: function(o) { return o && typeof o === 'object' },
-    defineBubblingProperties: function(object, key, parent) {
+    defineBubblingProperties: function(object, key, parent, deep) {
         Object.defineProperty(object, '__key', { value: key, enumerable: false, configurable: true })
         Object.defineProperty(object, '__parent', { value: parent, enumerable: false, configurable: true })
+        if(deep) {
+            Object.entries(object).forEach(function([key, val]) {
+                if(tools.isObj(val) && (!val.__key || !val.__parent))
+                    tools.defineBubblingProperties(object[key], key, object)
+            })
+        }
     }
 }
+
+export default tools
